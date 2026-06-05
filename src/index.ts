e3f6b2dba527a59db233e8eca5e627d81910befe
VB2007 <admin@vb2007.hu>
2026-06-06 01:13:39 +0200

    updated index.ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { MatrixAuth } from "@vector-im/matrix-bot-sdk";
+
+// This will be the URL where clients can reach your homeserver. Note that this might be different
+// from where the web/chat interface is hosted. The server must support password registration without
+// captcha or terms of service (public servers typically won't work).
+// const homeserverUrl: string = "https://matrix.vb2007.hu";
+//
+// const auth = new MatrixAuth(homeserverUrl);
+// const client = await auth.passwordLogin("matrixbot", "!49gd9hYtS$i8^Vj@n");
+//
+// console.log(
+//     "Copy this access token to your bot's config: ",
+//     client.accessToken
+// );
