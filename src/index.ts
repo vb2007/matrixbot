c12c38b5a53571b6b2b084dcdbf4cc8df058cc2b
VB2007 <admin@vb2007.hu>
2026-06-06 18:01:48 +0200

    updated index, added auth scripts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,43 @@
-import { MatrixAuth } from "@vector-im/matrix-bot-sdk";
+import {
+    MatrixClient,
+    SimpleFsStorageProvider,
+    AutojoinRoomsMixin,
+} from "@vector-im/matrix-bot-sdk";
 
 // This will be the URL where clients can reach your homeserver. Note that this might be different
 // from where the web/chat interface is hosted. The server must support password registration without
 // captcha or terms of service (public servers typically won't work).
-// const homeserverUrl: string = "https://matrix.vb2007.hu";
-//
-// const auth = new MatrixAuth(homeserverUrl);
-// const client = await auth.passwordLogin("matrixbot", "!49gd9hYtS$i8^Vj@n");
-//
-// console.log(
-//     "Copy this access token to your bot's config: ",
-//     client.accessToken
-// );
+const homeserverUrl = "https://example.org";
+
+// Use the access token you got from login or registration above.
+const accessToken = "ACQUIRED_FROM_ABOVE";
+
+// In order to make sure the bot doesn't lose its state between restarts, we'll give it a place to cache
+// any information it needs to. You can implement your own storage provider if you like, but a JSON file
+// will work fine for this example.
+const storage = new SimpleFsStorageProvider("hello-bot.json");
+
+// Finally, let's create the client and set it to autojoin rooms. Autojoining is typical of bots to ensure
+// they can be easily added to any room.
+const client = new MatrixClient(homeserverUrl, accessToken, storage);
+AutojoinRoomsMixin.setupOnClient(client);
+
+// Before we start the bot, register our command handler
+client.on("room.message", handleCommand);
+
+// Now that everything is set up, start the bot. This will start the sync loop and run until killed.
+client.start().then(() => console.log("Bot started!"));
+
+// This is the command handler we registered a few lines up
+async function handleCommand(roomId: string, event: any) {
+    // Don't handle unhelpful events (ones that aren't text messages, are redacted, or sent by us)
+    if (event["content"]?.["msgtype"] !== "m.text") return;
+    if (event["sender"] === (await client.getUserId())) return;
+
+    // Check to ensure that the `!hello` command is being run
+    const body = event["content"]["body"];
+    if (!body?.startsWith("!hello")) return;
+
+    // Now that we've passed all the checks, we can actually act upon the command
+    await client.replyNotice(roomId, event, "Hello world!");
+}
