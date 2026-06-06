06288b50aa4679cfe7e7a3413b504b096d65a402
VB2007 <admin@vb2007.hu>
2026-06-06 18:08:03 +0200

    fixed declaration order, added type safety

--- src/scripts/login.ts
+++ src/scripts/login.ts
@@ -1,12 +1,12 @@
-import { MatrixAuth } from "@vector-im/matrix-bot-sdk";
+import { MatrixAuth, MatrixClient } from "@vector-im/matrix-bot-sdk";
 
 // This will be the URL where clients can reach your homeserver. Note that this might be different
 // from where the web/chat interface is hosted. The server must support password registration without
 // captcha or terms of service (public servers typically won't work).
-const homeserverUrl = "https://example.org";
+const homeserverUrl: string = "https://example.org";
 
-const auth = new MatrixAuth(homeserverUrl);
-const client = await auth.passwordLogin("username", "password");
+const auth: MatrixAuth = new MatrixAuth(homeserverUrl);
+const client: MatrixClient = await auth.passwordLogin("username", "password");
 
 console.log(
     "Copy this access token to your bot's config: ",
