2e4aac4ff58b76c62154efcd7330f537a735d85d
VB2007 <admin@vb2007.hu>
2026-06-06 18:01:04 +0200

    created login & register scripts

---
+++ src/scripts/login.ts
@@ -0,0 +1,14 @@
+import { MatrixAuth } from "@vector-im/matrix-bot-sdk";
+
+// This will be the URL where clients can reach your homeserver. Note that this might be different
+// from where the web/chat interface is hosted. The server must support password registration without
+// captcha or terms of service (public servers typically won't work).
+const homeserverUrl = "https://example.org";
+
+const auth = new MatrixAuth(homeserverUrl);
+const client = await auth.passwordLogin("username", "password");
+
+console.log(
+    "Copy this access token to your bot's config: ",
+    client.accessToken
+);
