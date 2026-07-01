3cce0dd13ce138b960e5359ce693796a9230f9f4
VB2007 <admin@vb2007.hu>
2026-07-01 02:01:02 +0200

    added types to both commands

--- src/commands/echo.ts
+++ src/commands/echo.ts
@@ -1,6 +1,9 @@
-import { Command } from "../types/command";
+import { Command, CommandContext } from "../types/command";
 
 export const echoCommand: Command = {
     name: "echo",
     description: "Echoes back your input.",
+    async execute({ client, roomId, event }: CommandContext): Promise<void> {
+        const input: string = "";
+    },
 };
