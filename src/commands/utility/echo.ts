42c95ffb0b6e63c7f1751aceaa24a146f033b717
VB2007 <balazs.vagvolgyi@linistry.com>
2026-07-03 22:54:26 +0200

    added categories to commands

--- src/commands/utility/echo.ts
+++ src/commands/utility/echo.ts
@@ -1,7 +1,8 @@
-import { Command, CommandContext } from "../types/command";
+import { Command, CommandCategory, CommandContext } from "../../types/command";
 
 export const echoCommand: Command = {
     name: "echo",
+    category: CommandCategory.Utility,
     description: "Echoes back your input.",
     async execute({ client, roomId, event }: CommandContext): Promise<void> {
         const contentBody: string = event.content.body;
--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,8 +1,9 @@
-import { Command, CommandContext } from "../types/command";
+import { Command, CommandCategory, CommandContext } from "../../types/command";
 
 export const pingCommand: Command = {
     name: "ping",
     description: "Pong!",
+    category: CommandCategory.Utility,
     async execute({ client, roomId, event }: CommandContext): Promise<void> {
         await client.replyNotice(roomId, event, "Pong!");
     },
