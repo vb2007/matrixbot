11cd97a7180700a3ee8a84e2353da152825f2f8d
VB2007 <admin@vb2007.hu>
2026-07-06 16:34:36 +0200

    separated command loading & processing

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,7 @@ import {
 import { StoreType } from "@matrix-org/matrix-sdk-crypto-nodejs";
 
 import { ACCESS_TOKEN, HOMESERVER_URL } from "./helpers/dotenv";
-import { pingCommand } from "./commands/utility/ping";
-import { echoCommand } from "./commands/utility/echo";
-import { Command } from "./types/command";
+import { handleCommand } from "./helpers/commands/commandHandler";
 
 const storageProvider: SimpleFsStorageProvider = new SimpleFsStorageProvider(
     "./src/data/bot-store.json"
@@ -29,43 +27,6 @@ const client: MatrixClient = new MatrixClient(
 
 // Autojoining is typical of bots to ensure they can be easily added to any room.
 AutojoinRoomsMixin.setupOnClient(client);
-
-const allCommands: Command[] = [pingCommand, echoCommand];
-export const commandMap = new Map<string, Command>();
-
-for (const command of allCommands) {
-    commandMap.set(command.name, command);
-}
-
-const PREFIX = "!";
-
-const handleCommand = async (roomId: string, event: any) => {
-    // Exclude non-text , redacted, or bot's messages
-    if (event["content"]?.["msgtype"] !== "m.text") return;
-    if (event["sender"] === (await client.getUserId())) return;
-
-    const body: any = event["content"]["body"];
-    if (typeof body !== "string" || !body.startsWith(PREFIX)) return;
-
-    const raw: string = body.slice(PREFIX.length).trim();
-    if (!raw) return;
-
-    const [name, ...args] = raw.split(/\s+/);
-    const command: Command | undefined = commandMap.get(name.toLowerCase());
-    if (!command) return;
-
-    try {
-        await command.execute({ client, roomId, event, args });
-    } catch (err) {
-        console.error(`Command "${name}" failed:`, err);
-
-        await client.replyNotice(
-            roomId,
-            event,
-            "Something went wrong while executing that command."
-        );
-    }
-};
 
 client.on("room.message", handleCommand);
 
