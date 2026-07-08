4b1e73e9d0c38e5bb7598c8292b65f791ae66c3a
VB2007 <admin@vb2007.hu>
2026-07-08 12:33:35 +0200

    registered new command

--- src/helpers/commands/commandLoader.ts
+++ src/helpers/commands/commandLoader.ts
@@ -1,8 +1,9 @@
 import { pingCommand } from "../../commands/utility/ping";
 import { echoCommand } from "../../commands/utility/echo";
 import { Command } from "../../types/command";
+import { randompicCommand } from "../../commands/fun/randompic";
 
-const allCommands: Command[] = [pingCommand, echoCommand];
+const allCommands: Command[] = [pingCommand, echoCommand, randompicCommand];
 export const commandMap = new Map<string, Command>();
 
 for (const command of allCommands) {
