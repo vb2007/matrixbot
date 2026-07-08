39121d2e9090352a077ec176988b15161d57e44d
VB2007 <admin@vb2007.hu>
2026-07-08 22:07:13 +0200

    simplified return logic in echo.ts

--- src/commands/utility/echo.ts
+++ src/commands/utility/echo.ts
@@ -6,18 +6,20 @@ export const echoCommand: Command = {
     name: "echo",
     category: CommandCategory.Utility,
     description: "Echoes back your input.",
-    async execute({ client, roomId, event }: CommandContext): Promise<void> {
+    async execute({ client, roomId, event }: CommandContext): Promise<any> {
         const contentBody: string = event.content.body;
         const userInput: string = contentBody.split(
             `${PREFIX}${this.name} `
         )[1];
 
-        let response: string =
-            "Nothing to echo back. Enter some content after the command.";
-        if (userInput !== "" && userInput != undefined) {
-            response = userInput;
+        if (userInput === "" && userInput == undefined) {
+            return await client.replyNotice(
+                roomId,
+                event,
+                "Nothing to echo back. Enter some content after the command."
+            );
         }
 
-        await client.replyNotice(roomId, event, response);
+        return await client.replyNotice(roomId, event, userInput);
     },
 };
