dd7ff874b82402b86938bbbf158847678d6b919c
VB2007 <admin@vb2007.hu>
2026-07-08 12:23:50 +0200

    implemented poc for image reply

--- src/commands/fun/randompic.ts
+++ src/commands/fun/randompic.ts
@@ -5,5 +5,24 @@ export const randompicCommand: Command = {
     name: "randompic",
     category: CommandCategory.Fun,
     description: "Displays a random picture from picsum.photos.",
-    async execute({ client, roomId, event }: CommandContext): Promise<void> {},
+    async execute({ client, roomId, event }: CommandContext): Promise<void> {
+        const image = "https://picsum.photos/350";
+
+        await client.sendMessage(roomId, {
+            "m.relates.to": {
+                "m.in_reply_to": {
+                    event_id: event["event_id"],
+                },
+            },
+            msgtype: "m.image",
+            body: "random image",
+            url: "mxcUrl",
+            info: {
+                mimetype: "image/png",
+                imageBuffer.length,
+                w: 350,
+                h: 350
+            },
+        });
+    },
 };
