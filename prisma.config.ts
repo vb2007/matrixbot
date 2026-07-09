f676f7aa51d9097eee9b93cb1a2c126f99aeaeee
VB2007 <admin@vb2007.hu>
2026-07-09 19:55:32 +0200

    added shadow db url to prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,11 +4,12 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
-  },
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+    },
+    datasource: {
+        url: process.env["DATABASE_URL"],
+        shadowDatabaseUrl: process.env["SHADOW_DATABASE_URL"],
+    },
 });
