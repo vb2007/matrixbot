2d793488dade866bfbba6985afe672df6af1835c
VB2007 <admin@vb2007.hu>
2026-07-11 20:26:33 +0200

    finished POC for work.ts

--- src/commands/economy/work.ts
+++ src/commands/economy/work.ts
@@ -1,14 +1,27 @@
 import { Command, CommandContext } from "../../types/command";
 import { CommandCategory } from "../../types/commandCategory";
+import {
+    createUser,
+    doesUserExist,
+    updateBalance,
+} from "../../database/models/economy";
 
 export const workCommand: Command = {
     name: "work",
     category: CommandCategory.Economy,
     description: "Lets you work for a random amount of money.",
     async execute({ client, roomId, event }: CommandContext): Promise<any> {
-        const contentBody: string = event.content.body;
-        console.log(`Content Body: ${contentBody}`);
+        const senderUsername: string = event.sender;
 
-        return await client.replyNotice(roomId, event, "asd");
+        const amount: number = Math.floor(Math.random() * 100);
+
+        const isInDB: boolean = await doesUserExist(senderUsername);
+        if (!isInDB) {
+            await createUser(senderUsername, amount);
+        }
+
+        await updateBalance(senderUsername, amount, "increment");
+
+        return await client.replyNotice(roomId, event, `Worked, +${amount}`);
     },
 };
