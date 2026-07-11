358156619a0ae32e8479ed84ad03340f5d8e3496
VB2007 <admin@vb2007.hu>
2026-07-11 12:14:14 +0200

    created interfaces in advance

--- src/database/models/economy.ts
+++ src/database/models/economy.ts
@@ -1,4 +1,18 @@
 import { prismaClient } from "../prisma";
+// import { DateTimeFilter } from "../../../generated/prisma/commonInputTypes";
+//
+// export interface Economy {
+//     username: string;
+//     balance: number;
+//     firstInteractionTime: number;
+//
+//     economyActionTimes: EconomyActionTimes;
+// }
+//
+// export interface EconomyActionTimes {
+//     username: Economy["username"];
+//     lastWorkTime: DateTimeFilter;
+// }
 
 export const doesUserExist = async (username: string): Promise<boolean> => {
     const userQuery = await prismaClient.economy.findUnique({
