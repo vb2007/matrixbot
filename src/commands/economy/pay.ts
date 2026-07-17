e4b7cbc6b38046a71ebe8cc323a215ef9a9f4621
VB2007 <balazs.vagvolgyi@linistry.com>
2026-07-17 21:47:52 +0200

    created metadata for the pay command

---
+++ src/commands/economy/pay.ts
@@ -0,0 +1,11 @@
+import { Command, CommandContext } from "../../types/command";
+import { CommandCategory } from "../../types/commandCategory";
+
+export const payCommand: Command = {
+    name: "pay",
+    category: CommandCategory.Economy,
+    description: "Pays a set amount of credits to the specified user",
+    async execute({client, roomId, event}: CommandContext): Promise<any> {
+
+    }
+}
