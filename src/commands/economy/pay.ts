7a6c983525fefa3f379804b728dae076fc60e207
VB2007 <admin@vb2007.hu>
2026-07-20 23:57:28 +0200

    added contentBody, updated formatting

--- src/commands/economy/pay.ts
+++ src/commands/economy/pay.ts
@@ -5,7 +5,8 @@ export const payCommand: Command = {
     name: "pay",
     category: CommandCategory.Economy,
     description: "Pays a set amount of credits to the specified user",
-    async execute({client, roomId, event}: CommandContext): Promise<any> {
+    async execute({ client, roomId, event }: CommandContext): Promise<any> {
         const senderUsername: string = event.sender;
-    }
-}
+        const contentBody: string = event.content.body;
+    },
+};
