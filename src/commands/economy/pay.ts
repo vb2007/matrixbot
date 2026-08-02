3236e1cfe8a4df472d245d265f9efc3e69b94e01
VB2007 <admin@vb2007.hu>
2026-08-02 17:23:34 +0200

    created debug version for /pay

--- src/commands/economy/pay.ts
+++ src/commands/economy/pay.ts
@@ -1,5 +1,6 @@
 import { Command, CommandContext } from "../../types/command";
 import { CommandCategory } from "../../types/commandCategory";
+import { PREFIX } from "../../helpers/dotenv";
 
 export const payCommand: Command = {
     name: "pay",
@@ -7,6 +8,20 @@ export const payCommand: Command = {
     description: "Pays a set amount of credits to the specified user",
     async execute({ client, roomId, event }: CommandContext): Promise<any> {
         const senderUsername: string = event.sender;
+        const content: string = event.content;
         const contentBody: string = event.content.body;
+        const userInput: string = contentBody.split(
+            `${PREFIX}${this.name} `
+        )[1];
+
+        console.log(content);
+        console.log(senderUsername);
+        console.log(contentBody);
+
+        return await client.replyNotice(
+            roomId,
+            event,
+            `You've paid +${userInput} credits to xy.`
+        );
     },
 };
